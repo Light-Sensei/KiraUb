7d9bb022c2635d4fe0c14be9b96c585a9de9d339
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-02-14 21:08:45 +0530

    Update troll.py

--- userbot/assistant/troll.py
+++ userbot/assistant/troll.py
@@ -18,7 +18,7 @@ async def on_plug_in_callback_query_handler(event):
             ids = [userid]
             if event.query.user_id in ids:
                 reply_pop_up_alert = (
-                    "You are not allowed to see this message, better luck next time!"
+                    "Ma chuda bsdk lawde"
                 )
             else:
                 encrypted_tcxt = message["text"]
