6250be79c7ce79faf0b571627141de1110d26c15
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-02-14 21:14:53 +0530

    Update startup.py

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -61,9 +61,9 @@ async def startupmessage():
         if BOTLOG:
             Config.CATUBLOGO = await catub.tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/4e3ba8e8f7e535d5a2abe.jpg",
-                caption="**Your CatUserbot has been started successfully.**",
-                buttons=[(Button.url("Support", "https://t.me/catuserbot"),)],
+                "https://telegra.ph/file/87f0ab488a9a042902c95.jpg",
+                caption="**Your KiraUserbot has been started successfully.**",
+                buttons=[(Button.url("Support", "https://t.me/KiraUbSupport"),)],
             )
     except Exception as e:
         LOGS.error(e)
